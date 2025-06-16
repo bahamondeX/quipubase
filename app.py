79b2aafc267506165b31505ffd2467ea0292c0b0
bahamondex <oscar.bahamonde@oscarbahamonde.awsapps.com>
2025-06-16 01:01:08 -0500

    fc

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
 from google import genai
-from google.genai import types
 import os
-from openai.types.image_generate_params import ImageGenerateParams
-import random
-from quipubase.lib.
+from PIL import Image
+from io import BytesIO
 
-class ImageGenerationService:
-	"""
-	Service for generating images using Google's Imagen model.
-	Args:
-		api_key (str): Your Google API key for accessing the Imagen model.
-	"""
-	
-	
+def generate():
+    client = genai.Client(api_key=os.environ.get("OPENAI_API_KEY"))
+
+    result = client.models.generate_images(
+        model="models/imagen-3.0-generate-002",
+        prompt="""A 6/10 woman on her thirties with light skin tone, dark hair, big nose, high cheeks, oval face, big brown eyes with eyebrow on the bottom and a visible mole on her right cheekbone, straight flat-ironed black hair from a messy bun, wearing a black t-shirt with drums logo, staring at the camera of her fiance's iphone smiling very surprised with her hands crossed below her chin touching the back of her hands with her chin, sitting within a rock bar in Lima, Peru. Vintage style, 35mm film, high quality, detailed, realistic, cinematic lighting, soft focus""",
+        config=dict(
+            number_of_images=1,
+            output_mime_type="image/png",
+            person_generation="ALLOW_ADULT",
+            aspect_ratio="1:1",
+        ),
+    )
+
+    if not result.generated_images:
+        print("No images generated.")
+        return
+
+    if len(result.generated_images) != 1:
+        print("Number of images generated does not match the requested number.")
+
+    for generated_image in result.generated_images:
+        image = Image.open(BytesIO(generated_image.image.image_bytes))
+        image.save("1.png")
+
+
 if __name__ == "__main__":
     generate()
